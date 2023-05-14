def76aa85bc18ed64a6f8161081b08708ef1c2ab
Adrephos <adrephos@gmail.com>
2023-05-14 18:40:36 -0500

    feat: add LR(0) canonical automaton implementation

---
+++ src/automaton_states.go
@@ -0,0 +1,54 @@
+package src
+
+import (
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+// A state of a LR(0) automaton with kernel and clossure
+type state struct {
+	Kernel sets.Set[argument]
+	Clossure sets.Set[argument]
+}
+
+// Returns a new state for an LR(0) automaton for a given kernel set
+func NewState(g grammar, k sets.Set[argument]) state {
+	kernel := k		
+	clossure := k.Clone()	
+	var nTerminal []string
+	alreadyVisited := sets.NewString()
+	for item := range clossure {
+		transition := item.GetTransition()
+		if g.N.Has(transition) {
+			nTerminal = append(nTerminal, transition)
+		}
+	}
+	if len(nTerminal) == 0 { return state{ kernel, clossure }}
+
+	index := 0; finishFlag := true
+
+	for finishFlag {
+		current := nTerminal[index]
+		if alreadyVisited.Has(current) {
+			continue
+		} else {
+			alreadyVisited.Insert(current)
+		}
+
+		for _, production := range g.P[current] {
+			arg := newArg(current, production, 0)
+			transition := arg.GetTransition()
+			clossure.Insert(arg)
+
+			if g.N.Has(transition) && !alreadyVisited.Has(transition) {
+				nTerminal = append(nTerminal, transition)
+			}
+		}
+		if index == len(nTerminal) - 1 {
+			finishFlag = false
+		} else {
+			index++
+		}
+	}
+
+	return state{ kernel, clossure }
+}
