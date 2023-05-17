aa6e35dc7a4c5ad3fbe8fd02a2d0c684c591f01e
Adrephos <adrephos@gmail.com>
2023-05-17 15:05:40 -0500

    feat: add file input and move routines to its own file

--- main.go
+++ main.go
@@ -1,56 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
-	"github.com/Adrephos/top-down/src"
-	//"k8s.io/apimachinery/pkg/util/sets"
+	"github.com/Adrephos/syntax-analysis/src"
 )
 
 func main() {
-	grammar := src.GrammarInput()
 
-	stringArr := src.StringsInput()
+	argsWithoutProg := os.Args[1:]
 
-	leftRec := false
-	fmt.Printf("\nTop down parser\n\n")
-	// LL1 parse routine
-	if grammar.HasLeftRecursion() {
-		fmt.Println("Error: La gramática tiene recursividad por la izquierda")
-		leftRec = true
-	}
 
-	if grammar.IsLL1() && !leftRec {
-		for _, value := range stringArr {
-			isPartOfGrammar, err := src.PredictiveParsing(grammar, value)
-			if isPartOfGrammar {
-				fmt.Printf("La cadena %s SI hace parte de la gramática\n", value)
-			} else {
-				fmt.Printf("La cadena %s NO hace parte de la gramática\n", value)
-				fmt.Printf(err.Error())
-			}
-		}
+	if len(argsWithoutProg) == 0 {
+
+		grammar := src.GrammarInput()
+		strings := src.StringsInput()
+
+		src.Ll1Routine(grammar, strings)
+		src.Lr0Routine(grammar, strings)
+
 	} else {
-		fmt.Println("Error: Esta gramática no es LL(1)")
-	}
-
-	fmt.Printf("\n\nBottom up parser\n\n")
-	// LR parse routine
-	action, arguments, err := grammar.CreateSLRTable()
-	if err != nil {
-		fmt.Printf("Error: La gramática no es LR(0)\n")
-		fmt.Printf(err.Error())
-	} else {
-		for _, value := range stringArr {
-			isPartOfGrammar, err := src.LRParsing(grammar, value, action, arguments, err)
-			if isPartOfGrammar {
-				fmt.Printf("La cadena %s SI hace parte de la gramática\n", value)
-			} else {
-				fmt.Printf("La cadena %s NO hace parte de la gramática\n", value)
-				if err != nil {
-					fmt.Printf(err.Error())
-				}
-			}
-		}
+		
+		src.FileInput(argsWithoutProg[0])
 	}
 }
